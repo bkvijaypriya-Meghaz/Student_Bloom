cd11e1df25c558d018191bd550902e371167ca7a
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-04 21:47:18 +0530

    Integrate the API My Teacher section on Student Dashboard

--- pages/student/ConstFile.js
+++ pages/student/ConstFile.js
@@ -1,5 +1,6 @@
 export const STUDENT_PROFILE_URL = "https://bloomlms.azure-api.net/student/v1/1/profile"
 export const STUDENT_UPCOMING_ASSIGNMENT = "https://bloomlms.azure-api.net/student/v1/1/assignments/upcoming"
+export const MY_TEACHER_INFO = "https://bloomlms.azure-api.net/student/v1/1/teacher/profile"
 
 const dummyComponent = () => {
     return(
--- pages/student/studentsTeacher.js
+++ pages/student/studentsTeacher.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import { FiLock } from 'react-icons/fi';
+import {MY_TEACHER_INFO} from './ConstFile';
+import {useState, useEffect} from 'react'
+import {getInfo} from '../../services/GenericServices';
 
 export default function studentsTeacher() {
+
+    const [myTeacherInfo, setMyTeacherInfo] = useState(0);
+
+    useEffect(() => {
+        getInfo(MY_TEACHER_INFO).then((data) => {
+            // console.log('bloom data is : ',data);
+            // console.log('bloom data is : ',data.lastName);
+            // console.log('bloom data is : ',data.firstName);
+            setMyTeacherInfo(data);
+        })
+    }, [])
+
     return (
         <div className="">
             <div className="card">
@@ -9,7 +24,11 @@ export default function studentsTeacher() {
                     <h5>My Teacher</h5>
                 </div>
                 <div className="card-block stuprofile_card">
-                    <div className="teacherimg"> <span data-toggle="tooltip" data-html="true" data-placement="bottom" title="Ms. Jane Doe">JD</span></div>
+                    <div className="teacherimg"> 
+                        <span data-toggle="tooltip" data-html="true" data-placement="bottom" title="Ms. Jane Doe">
+                                {myTeacherInfo.lastName}{myTeacherInfo.firstName}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
