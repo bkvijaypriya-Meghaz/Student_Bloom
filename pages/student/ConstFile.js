04ed01619928f6b5ba03bded8c8146df8090d9b8
VictoryVP <victoryvp@users.noreply.github.com>
2020-11-04 15:56:55 +0530

    Build failed for the constfile and so added a dummy export function and build is working

--- pages/student/ConstFile.js
+++ pages/student/ConstFile.js
@@ -1,2 +1,10 @@
 export const STUDENT_PROFILE_URL = "https://bloomlms.azure-api.net/student/v1/1/profile"
 export const STUDENT_UPCOMING_ASSIGNMENT = "https://bloomlms.azure-api.net/student/v1/1/assignments/upcoming"
+
+const dummyComponent = () => {
+    return(
+        <>
+        </>
+    )
+  }
+export default dummyComponent
